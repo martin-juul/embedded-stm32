6b3f3e76b7e859e7bf544c6f1333c77bbb71474c
Martin Juul <martin@sitetech.dk>
2019-01-10 11:22:47 +0100

    Add earthquake led, refactor run_temp_humid()

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <mbed.h>
 #include "LCD_DISCO_F746NG.h"
 #include "DHT.h"
+#include <iostream>
 
 LCD_DISCO_F746NG lcd;
 DigitalOut led1(LED1);
+DigitalOut led2(D2);
 
 DHT sensor(A0, SEN51035P);
 AnalogIn vibrationSensor(A1);
 AnalogIn soundSensor(A2);
+
+Serial usbCom(USBTX, USBRX);
 
 void boot()
 {
@@ -16,7 +20,9 @@ void boot()
 
   lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"Booting Hercules...", CENTER_MODE);
   lcd.DisplayStringAt(0, LINE(4), (uint8_t *)"Please wait", CENTER_MODE);
+
   wait(1);
+
   lcd.Clear(LCD_COLOR_DARKCYAN);
 }
 
@@ -24,8 +30,12 @@ int is_earth_quake()
 {
   if (vibrationSensor.read() > 0.06)
   {
+    led2 = 1;
+
     return 1;
   }
+
+  led2 = 0;
 
   return 0;
 }
@@ -36,7 +46,7 @@ void run_temp_humid()
   int error = 0;
   float h = 0.0f, c = 0.0f;
 
-  wait(2.0f);
+  //wait(2.0f);
   error = sensor.readData();
   if (0 == error)
   {
@@ -46,15 +56,15 @@ void run_temp_humid()
     char temp[32];
     char humid[32];
 
-    sprintf(temp, "Temperature: %f", c);
-    sprintf(humid, "Humidity: %f", h);
+    sprintf(temp, "Temperature: %d c", (int)c);
+    sprintf(humid, "Humidity: %d %%", (int)h);
 
     lcd.DisplayStringAt(0, LINE(7), (uint8_t *)temp, CENTER_MODE);
     lcd.DisplayStringAt(0, LINE(8), (uint8_t *)humid, CENTER_MODE);
   }
   else
   {
-    printf("Error: %d\n", error);
+    //usbCom.printf("Error: %d\n", error);
   }
 
   char earth_quake[32];
@@ -67,14 +77,11 @@ int counter_current = 0;
 
 void run_counter()
 {
-  if (counter_max <= counter_current)
+  //usbCom.printf("%d max\n", counter_max);
+  //usbCom.printf("%d current\n", counter_current);
+
+  if (counter_current <= counter_max)
   {
-    uint32_t count = counter_current;
-    char display_int[32];
-    sprintf(display_int, "Count: %u", count);
-
-    lcd.DisplayStringAt(0, LINE(6), (uint8_t *)display_int, CENTER_MODE);
-
     counter_current++;
 
     if (counter_current == counter_max)
@@ -82,6 +89,11 @@ void run_counter()
       counter_current = 0;
     }
   }
+
+  char display_int[32];
+  sprintf(display_int, "Count: %u", counter_current);
+
+  lcd.DisplayStringAt(0, LINE(5), (uint8_t *)display_int, CENTER_MODE);
 }
 
 int main()
